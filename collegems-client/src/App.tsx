454165e5806dc039ace37a5b93b145f134d4f23d
imanchalsingh <singhanchal159357@gmail.com>
2026-02-07 19:14:54 +0530

    fix : added routes - client

--- collegems-client/src/App.tsx
+++ collegems-client/src/App.tsx
@@ -6,15 +6,25 @@ import RoleRoute from "./components/RoleRoute";
 import StudentDashboard from "./pages/students/StudentDashboard";
 import TeacherDashboard from "./pages/teacher/TeacherDashboard";
 import HodDashboard from "./pages/hod/HODDashboard";
+import MainDashboard from "./pages/MainDashboard";
+import ExamSchedule from "./pages/students/ExamSchedule";
+import Courses from "./pages/students/Courses";
+import Teachers from "./components/Teachers";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<MainDashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
 
+        <Route path="/examschedule" element={<ExamSchedule />} />
+        <Route path="/results" element={<div>Results Page</div>} />
+        <Route path="/events" element={<div>Events Page</div>} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/timetable" element={<div>Timetable Page</div>} />
+        <Route path="/faculty" element={<Teachers/>} />
         <Route
           path="/student/dashboard"
           element={
