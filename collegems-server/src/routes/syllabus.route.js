9a3da0417de988eede6c6237cc4cacb84c9fd657
imanchalsingh <singhanchal159357@gmail.com>
2026-02-15 09:40:51 +0530

    feat : syllabus route

---
+++ collegems-server/src/routes/syllabus.route.js
@@ -0,0 +1,28 @@
+import express from "express";
+import Syllabus from "../models/Syllabus.js";
+import authMiddleware from "../middleware/auth.js";
+
+const router = express.Router();
+
+// Create syllabus (HOD + Teacher only)
+router.post("/", authMiddleware, async (req, res) => {
+  if (!["hod", "teacher"].includes(req.user.role)) {
+    return res.status(403).json({ message: "Not allowed" });
+  }
+
+  const syllabus = new Syllabus({
+    ...req.body,
+    createdBy: req.user.id,
+  });
+
+  await syllabus.save();
+  res.json(syllabus);
+});
+
+// Get syllabus (All roles)
+router.get("/", authMiddleware, async (req, res) => {
+  const data = await Syllabus.find();
+  res.json(data);
+});
+
+export default router;
