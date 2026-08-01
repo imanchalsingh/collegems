57080fbd12d584c412e26819f3ff7d85c79bc0b2
suman panda <pandasuman12345@gmail.com>
2026-08-01 12:26:15 +0530

    multi-factor authentication for CollegeMS accounts

--- collegems-server/src/routes/index.js
+++ collegems-server/src/routes/index.js
@@ -8,6 +8,7 @@ import express from "express";
 
 // Auth & Core
 import authRoutes from "./auth.routes.js";
+import mfaRoutes from "./mfa.routes.js";
 import dashboardRoutes from "./dashboard.routes.js";
 import userRoutes from "./user.routes.js";
 import historyRoutes from "./history.routes.js";
@@ -94,6 +95,7 @@ import temporaryLinkRoutes from "./temporaryLink.routes.js";
 // MIDDLEWARES
 // ========================================
 import { authenticate } from "../middlewares/auth.middleware.js";
+import { mfaGuard } from "../middlewares/mfaGuard.js";
 import { verifyStudent } from "../controllers/idcard.controller.js";
 
 // ========================================
@@ -105,6 +107,7 @@ const router = express.Router();
 // PUBLIC & EXCEPTION ROUTES
 // ========================================
 router.use("/auth", authRoutes);
+router.use("/mfa", mfaRoutes);
 router.use("/assignment", assignmentRoutes);
 router.use("/temporary-links", temporaryLinkRoutes);
 
@@ -113,6 +116,7 @@ router.use("/temporary-links", temporaryLinkRoutes);
 // ========================================
 const authenticatedRouter = express.Router();
 authenticatedRouter.use(authenticate);
+authenticatedRouter.use(mfaGuard);
 
 // Core Routes
 authenticatedRouter.use("/search", searchRoutes);
